b3ab3be791219bf8bc86d912d0c1c6d7ed2b3b72
oucyan420 <kingleim0@hotmail.co.jp>
2024-10-26 02:25:04 +0900

    Update reizouplagmini.py

--- reizouplagmini.py
+++ reizouplagmini.py
@@ -1,9 +1,15 @@
+import os
 import requests
 import time
 import uuid
 import hmac
 import hashlib
 import base64
+
+# 環境変数からトークンとシークレットキーを読み込む
+api_token = os.getenv('API_TOKEN')
+secret_token = os.getenv('SECRET_TOKEN')
+device_id = os.getenv('DEVICE_ID')
 
 def send_command(api_token, secret_token, device_id, command):
     url = f"https://api.switch-bot.com/v1.1/devices/{device_id}/commands"
@@ -31,17 +37,12 @@ def send_command(api_token, secret_token, device_id, command):
     response = requests.post(url, headers=headers, json=body)
     print(f"Sending {command} command: {response.text}")
 
-# トークンとシークレットキーを設定
-api_token = 'b49874c4f3af64b1e6bb754bc10a703ec441878739bfd549642f5d4041e271a2bf7834cf27a8969af5b909d558d1e7e1'
-secret_token = '1d58b22e63cd0e2a1075677ab6085de7'
-device_id = '6055F927277E'
-
 # 30分ごとにON/OFFを切り替える
 while True:
-    # プラグ電源オフ
-    send_command(api_token, secret_token, device_id, "turnOff")
-    time.sleep(30 * 60)  # 30分待機
-
     # プラグ電源オン
     send_command(api_token, secret_token, device_id, "turnOn")
+    time.sleep(30 * 60)  # 30分待機
+
+    # プラグ電源オフ
+    send_command(api_token, secret_token, device_id, "turnOff")
     time.sleep(30 * 60)  # 30分待機
